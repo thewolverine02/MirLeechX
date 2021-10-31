7137ab6b221a7652d013b7e7ca26b868f8c8e60d
thewolverine02 <93479218+thewolverine02@users.noreply.github.com>
2021-10-31 19:42:36 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -45,8 +45,8 @@ def stats(update, context):
 
 def start(update, context):
     buttons = button_build.ButtonMaker()
-    buttons.buildbutton("Repo", "https://github.com/iamLiquidX/MirLeechX")
-    buttons.buildbutton("Group", "https://t.me/mirrorLeechTelegramBot")
+    buttons.buildbutton("support", "https://t.me/LinkZz_MBBS")
+    buttons.buildbutton("Group", "https://t.me/LinkZz_MBBS")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
@@ -142,8 +142,8 @@ help_string_telegraph = f'''<br>
 <b>/{BotCommands.StatsCommand}</b>: Show Stats of the machine the bot is hosted on
 '''
 help = Telegraph(access_token=telegraph_token).create_page(
-        title='MirLeechX Help',
-        author_name='MirLeechX',
+        title='LinkZz_MBBS Help',
+        author_name='LinkZz_MBBS',
         author_url='https://github.com/iamLiquidX/MirLeechX',
         html_content=help_string_telegraph,
     )["path"]
